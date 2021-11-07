eaac2ced8af0a819b85a73bb8851c925df0760c4
Joshua Schlichting <7680545+JoshuaSchlichting@users.noreply.github.com>
2021-11-07 12:03:38 -0500

    update/add build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,22 @@
-go build -o bin/cert-installer .
-chmod +x bin/cert-installer
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    OS=linux
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    OS=macos
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+    OS=cygwin
+elif [[ "$OSTYPE" == "msys" ]]; then
+    # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+    OS=mysys
+elif [[ "$OSTYPE" == "win32" ]]; then
+    OS=windows
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+    OS=freebsd
+else
+    OS=unknown
+fi
+
+echo $OS
+BIN_FILENAME=bin/cert-installer_$(echo $OS)_$(uname -m)
+echo FILENAME = $BIN_FILENAME
+go build -o $BIN_FILENAME .
+chmod +x $BIN_FILENAME
---
+++ scripts/build_linux_x86_64.sh
@@ -0,0 +1,1 @@
+env GOOS=linux GOARCH=amd64 go build -o bin/cert-installer_linux_x86_64 .
